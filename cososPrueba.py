9d10a2d158c65cc83ad735e1b7b2074ce6489bb0
klyct <takanashirikka.42@gmail.com>
2023-05-31 19:47:01 -0600

    Ventana de grabacion

--- cososPrueba.py
+++ cososPrueba.py
@@ -1,53 +1,120 @@
-#words = nltk.word_tokenize(sample)
-#print('tokenizado: \n',words)
+##print('\nLemmatized:\n', lemmas)
 
-def remove_non_ascii(words):
-    """Remove non-ASCII characters from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-        new_words.append(new_word)
-    return new_words
 
-def replace_numbers(words):
-    """Replace all interger occurrences in list of tokenized words with textual representation"""
-    p = inflect.engine()
-    new_words = []
-    for word in words:
-        if word.isdigit():
-            new_word = p.number_to_words(word)
-            new_words.append(new_word)
-        else:
-            new_words.append(word)
-    return new_words
+r = sr.Recognizer()
 
-def normalize(words):
-    words = remove_non_ascii(words)
-    words = replace_numbers(words)
-    return words
+# uso de microfono
+with sr.Microphone(device_index=1) as source2:
+    # obtencion de audio
+    print("recording")
+    # apagar microfo 2s despues de un silencio, en ruido ambiental
+    r.adjust_for_ambient_noise(source2, duration=0.2)
+    audio2 = r.listen(source2)
+    print("done")
 
-#words = normalize(words)
-#print('Normalizado \n',words)
+    # conversion voz -> txt
+    MyText = r.recognize_google(audio2, language='es-MX')
+    MyText = MyText.lower() ## en minusculas
 
-def stem_words(words):
-    """Stem words in list of tokenized words"""
-    stemmer = SnowballStemmer('spanish')
-    stems = [stemmer.stem(i) for i in words]
-    return stems
+    # Mostrar en consola
+    print("Texto reconocido: \n" + MyText)
 
-def lemmatize_verbs(words):
-    """Lemmatize verbs in list of tokenized words"""
 
-    #nltk.download('wordnet')
-    lemmatizer = WordNetLemmatizer()
-    lemmas = [lemmatizer.lemmatize(i) for i in words]
-    return lemmas
+def remove_between_square_brackets(text):
+    # Elimina los caracteres
+    return re.sub('\[[^]]*\]', '', text)
 
-def stem_and_lemmatize(words):
-    stems = stem_words(words)
-    lemmas = lemmatize_verbs(words)
-    return stems, lemmas
 
-#stems, lemmas = stem_and_lemmatize(words)
-#print('Stemmed:\n', stems)
-#print('\nLemmatized:\n', lemmas)
+def replace_contractions(text):
+    # Reemplaza las contracciones por a version
+    # extendida de las palabras
+    return contractions.fix(text)
+
+
+def denoise_text(text):
+    text = remove_between_square_brackets(text)
+    text = replace_contractions(text)
+    return text
+
+
+MyText = denoise_text(MyText)
+print("Denoise: \n" + MyText)
+
+# stanza.download("es")
+##definimos el idioma----------#Definimos los procesos que se estaran utilizando
+nlp = stanza.Pipeline(lang='es', processors='tokenize,mwt,pos,lemma,depparse')
+# se maneja en formato de documento por lo tanto mandamos nuestra muestra
+doc = nlp(MyText)
+
+##Obtenemos las palabras lemmatizadas
+palabras=[f'{word.lemma}' for sent in doc.sentences for word in sent.words]
+print(palabras)
+
+##Creamos el diccionario
+lsm = {"abeja": 'abeja.mp4',
+       "abandono": 'abandonar.mp4',
+       "abandonar": 'abandonar.mp4',
+       "abandonado": 'abandonar.mp4',
+       "abogado": 'abogado.mp4',
+       'abrazo': 'abrazo.mp4',
+       'abreviatura': 'abreviatura.mp4',
+       "abril": 'abril.mp4',
+       'absorber': 'absorber.mp4',
+       'abstener': 'abstener.mp4',
+       'abstenerse': 'abstenerse.mp4',
+       'absurdo': 'absurdo.mp4',
+       'abuela': 'abuela.mp4',
+       'abuelo': 'abuelo.mp4',
+       'acabar': 'acabar.mp4',
+       'aceite': 'aceite.mp4',
+       'acelera': 'acelera.mp4',
+       'acerca': 'acerca.mp4',
+       'aclaracion': 'aclaracion.mp4',
+       'aclarar': 'aclarar.mp4',
+       'acordar': 'acordar.mp4',
+       'acostumbrar': 'acostumbrar.mp4',
+       'actividad': 'actividad.mp4',
+       'actor': 'actor.mp4',
+       'actual': 'actual.mp4',
+       'acuerdo': 'acuerdo.mp4',
+       'ahora': 'ahora.mp4',
+       'artista': 'artista.mp4',
+       'claramente': 'claramente.mp4',
+       'claro': 'claro.mp4',
+       'costumbre': 'costumbre.mp4',
+       'obviamente': 'obviamente.mp4',
+       'obvio': 'obvio.mp4',
+       'presente': 'presente.mp4',
+       'ridiculo': 'ridiculo.mp4',
+       'sobre': 'sobre.mp4',
+       'terminar': 'terminar.mp4',
+       'auto': 'auto.mp4',
+       'automóvil': 'automóvil.mp4',
+       'coche': 'coche.mp4',
+       'comer': 'comer.mp4',
+       'comida': 'comida.mp4',
+       'reclamar': 'reclamar.mp4',
+       'reprochar': 'reprochar.mp4',
+       'vehículo': 'vehículo.mp4',
+       'viento': 'viento.mp4',
+       }
+
+for nom in palabras:
+    nomVideo = lsm[nom]                 ##envio de palabras mediante lista
+    print(nomVideo)##control
+    video = cv2.VideoCapture(nomVideo)  ##obtener video del diccionario
+    while video.isOpened():             ##reproduccion del video
+        ret, imagen = video.read()
+        if ret == False:                ##no hay mas cuadros
+            break
+        cv2.imshow('video', imagen)     ##reproduce el video a
+        if cv2.waitKey(30) == ord('s'):  ##30 fps  con 's' como tecla de escape
+            break
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,123 +1,126 @@
 import speech_recognition as sr ##speech recognition
+import tkinter as tk
 import re
 import contractions
 import stanza
 import cv2 ##opencv-phyton
+def start_recording():
+    r = sr.Recognizer()
 
-r = sr.Recognizer()
+    # Uso de micrófono
+    with sr.Microphone(device_index=1) as source2:
+        # Obtención de audio
+        print("Recording")
+        # Apagar el micrófono 2s después de un silencio, en ruido ambiental
+        r.adjust_for_ambient_noise(source2, duration=0.2)
+        audio2 = r.listen(source2)
+        print("Done")
 
-# uso de microfono
-with sr.Microphone(device_index=1) as source2:
-    # obtencion de audio
-    print("recording")
-    # apagar microfo 2s despues de un silencio, en ruido ambiental
-    r.adjust_for_ambient_noise(source2, duration=0.2)
-    audio2 = r.listen(source2)
-    print("done")
+        # Conversión voz -> texto
+        MyText = r.recognize_google(audio2, language='es-MX')
+        MyText = MyText.lower()  # En minúsculas
 
-    # conversion voz -> txt
-    MyText = r.recognize_google(audio2, language='es-MX')
-    MyText = MyText.lower() ## en minusculas
+        # Mostrar en consola
+        print("Texto reconocido:\n" + MyText)
 
-    # Mostrar en consola
-    print("Texto reconocido: \n" + MyText)
+        # Realizar las operaciones adicionales con el texto reconocido
 
+        def remove_between_square_brackets(text):
+            # Elimina los caracteres
+            return re.sub('\[[^]]*\]', '', text)
 
-def remove_between_square_brackets(text):
-    # Elimina los caracteres
-    return re.sub('\[[^]]*\]', '', text)
+        def replace_contractions(text):
+            # Reemplaza las contracciones por la versión extendida de las palabras
+            return contractions.fix(text)
 
+        def denoise_text(text):
+            text = remove_between_square_brackets(text)
+            text = replace_contractions(text)
+            return text
 
-def replace_contractions(text):
-    # Reemplaza las contracciones por a version
-    # extendida de las palabras
-    return contractions.fix(text)
+        MyText = denoise_text(MyText)
+        print("Denoise:\n" + MyText)
 
+        # stanza.download("es")
+        ## Definimos el idioma----------#Definimos los procesos que se estarán utilizando
+        nlp = stanza.Pipeline(lang='es', processors='tokenize,mwt,pos,lemma,depparse')
+        # se maneja en formato de documento por lo tanto mandamos nuestra muestra
+        doc = nlp(MyText)
 
-def denoise_text(text):
-    text = remove_between_square_brackets(text)
-    text = replace_contractions(text)
-    return text
+        ## Obtenemos las palabras lematizadas
+        palabras = [f'{word.lemma}' for sent in doc.sentences for word in sent.words]
+        print(palabras)
 
+        ## Creamos el diccionario
+        lsm = {
+            "abeja": 'abeja.mp4',
+            "abandono": 'abandonar.mp4',
+            "abandonar": 'abandonar.mp4',
+            "abandonado": 'abandonar.mp4',
+            "abogado": 'abogado.mp4',
+            'abrazo': 'abrazo.mp4',
+            'abreviatura': 'abreviatura.mp4',
+            "abril": 'abril.mp4',
+            'absorber': 'absorber.mp4',
+            'abstener': 'abstener.mp4',
+            'abstenerse': 'abstenerse.mp4',
+            'absurdo': 'absurdo.mp4',
+            'abuela': 'abuela.mp4',
+            'abuelo': 'abuelo.mp4',
+            'acabar': 'acabar.mp4',
+            'aceite': 'aceite.mp4',
+            'acelera': 'acelera.mp4',
+            'acerca': 'acerca.mp4',
+            'aclaracion': 'aclaracion.mp4',
+            'aclarar': 'aclarar.mp4',
+            'acordar': 'acordar.mp4',
+            'acostumbrar': 'acostumbrar.mp4',
+            'actividad': 'actividad.mp4',
+            'actor': 'actor.mp4',
+            'actual': 'actual.mp4',
+            'acuerdo': 'acuerdo.mp4',
+            'ahora': 'ahora.mp4',
+            'artista': 'artista.mp4',
+            'claramente': 'claramente.mp4',
+            'claro': 'claro.mp4',
+            'costumbre': 'costumbre.mp4',
+            'obviamente': 'obviamente.mp4',
+            'obvio': 'obvio.mp4',
+            'presente': 'presente.mp4',
+            'ridiculo': 'ridiculo.mp4',
+            'sobre': 'sobre.mp4',
+            'terminar': 'terminar.mp4',
+            'auto': 'auto.mp4',
+            'automóvil': 'automóvil.mp4',
+            'coche': 'coche.mp4',
+            'comer': 'comer.mp4',
+            'comida': 'comida.mp4',
+            'reclamar': 'reclamar.mp4',
+            'reprochar': 'reprochar.mp4',
+            'vehículo': 'vehículo.mp4',
+            'viento': 'viento.mp4',
+        }
 
-MyText = denoise_text(MyText)
-print("Denoise: \n" + MyText)
+        for nom in palabras:
+            nomVideo = lsm[nom]  ## Envío de palabras mediante lista
+            print(nomVideo)  ## Control
+            video = cv2.VideoCapture(nomVideo)  ## Obtener video del diccionario
+            while video.isOpened():  ## Reproducción del video
+                ret, imagen = video.read()
+                if ret == False:  ## No hay más cuadros
+                    break
+                cv2.imshow('video', imagen)  ## Reproduce el video a
+                if cv2.waitKey(30) == ord('s'):  ## 30 fps  con 's' como tecla de escape
+                    break
 
-# stanza.download("es")
-##definimos el idioma----------#Definimos los procesos que se estaran utilizando
-nlp = stanza.Pipeline(lang='es', processors='tokenize,mwt,pos,lemma,depparse')
-# se maneja en formato de documento por lo tanto mandamos nuestra muestra
-doc = nlp(MyText)
+# Crear la interfaz gráfica
+root = tk.Tk()
+root.title("Grabación de Audio")
+root.geometry("300x100")
 
-##Obtenemos las palabras lemmatizadas
-palabras=[f'{word.lemma}' for sent in doc.sentences for word in sent.words]
-print(palabras)
+# Botón de inicio de grabación
+record_button = tk.Button(root, text="Iniciar grabación", command=start_recording)
+record_button.pack()
 
-##Creamos el diccionario
-lsm = {"abeja": 'abeja.mp4',
-       "abandono": 'abandonar.mp4',
-       "abandonar": 'abandonar.mp4',
-       "abandonado": 'abandonar.mp4',
-       "abogado": 'abogado.mp4',
-       'abrazo': 'abrazo.mp4',
-       'abreviatura': 'abreviatura.mp4',
-       "abril": 'abril.mp4',
-       'absorber': 'absorber.mp4',
-       'abstener': 'abstener.mp4',
-       'abstenerse': 'abstenerse.mp4',
-       'absurdo': 'absurdo.mp4',
-       'abuela': 'abuela.mp4',
-       'abuelo': 'abuelo.mp4',
-       'acabar': 'acabar.mp4',
-       'aceite': 'aceite.mp4',
-       'acelera': 'acelera.mp4',
-       'acerca': 'acerca.mp4',
-       'aclaracion': 'aclaracion.mp4',
-       'aclarar': 'aclarar.mp4',
-       'acordar': 'acordar.mp4',
-       'acostumbrar': 'acostumbrar.mp4',
-       'actividad': 'actividad.mp4',
-       'actor': 'actor.mp4',
-       'actual': 'actual.mp4',
-       'acuerdo': 'acuerdo.mp4',
-       'ahora': 'ahora.mp4',
-       'artista': 'artista.mp4',
-       'claramente': 'claramente.mp4',
-       'claro': 'claro.mp4',
-       'costumbre': 'costumbre.mp4',
-       'obviamente': 'obviamente.mp4',
-       'obvio': 'obvio.mp4',
-       'presente': 'presente.mp4',
-       'ridiculo': 'ridiculo.mp4',
-       'sobre': 'sobre.mp4',
-       'terminar': 'terminar.mp4',
-       'auto': 'auto.mp4',
-       'automóvil': 'automóvil.mp4',
-       'coche': 'coche.mp4',
-       'comer': 'comer.mp4',
-       'comida': 'comida.mp4',
-       'reclamar': 'reclamar.mp4',
-       'reprochar': 'reprochar.mp4',
-       'vehículo': 'vehículo.mp4',
-       'viento': 'viento.mp4',
-       }
-
-for nom in palabras:
-    nomVideo = lsm[nom]                 ##envio de palabras mediante lista
-    print(nomVideo)##control
-    video = cv2.VideoCapture(nomVideo)  ##obtener video del diccionario
-    while video.isOpened():             ##reproduccion del video
-        ret, imagen = video.read()
-        if ret == False:                ##no hay mas cuadros
-            break
-        cv2.imshow('video', imagen)     ##reproduce el video a
-        if cv2.waitKey(30)== ord('s'):  ##30 fps  con 's' como tecla de escape
-            break
-
-
-
-
-
-
-
-
+# Ejecutar la aplicación
+root.mainloop()
